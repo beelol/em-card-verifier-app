3e310b180cf5db67898948af149bd70c6fb2674c
Bilal Itani <bilalitani1@gmail.com>
2024-11-26 15:23:34 -0600

    Fix padding on 320px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-0 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex justify-center w-full">
           <div className="flex-col items-center justify-center">
