fde2ea07d5ae68f1ef9b0ddad0b708f4964e24e7
Bilal Itani <bilalitani1@gmail.com>
2024-11-26 14:11:44 -0600

    Move toasts to top center

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ToastContainer />
+        <ToastContainer position="top-center" />
 
         <VerificationProvider>{children}</VerificationProvider>
       </body>
